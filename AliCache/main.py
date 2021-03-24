043a7ac3dadddb9df8915d9699eecfc7cbdbf430
Ali Negary <ali.negary.76@gmail.com>
2021-03-24 18:38:10 +0430

    little progress in optimal (belady)

--- AliCache/main.py
+++ AliCache/main.py
@@ -19,6 +19,8 @@ Please enter the input arguments as follows:
 
 # step 1 - define the main method:
 def chooseCache(input_path, block_size=64, cache_size=32, cache_associativity=2, replacement_policy="lru"):
+    # begin timing the code
+    start_time = time.time()
     input_file = open(input_path, 'r')
     hex_address_list = input_file.readlines()
     binary_address_list = [bin(int(hex_num, 16))[2:] for hex_num in hex_address_list]  # convert hex to binary
@@ -36,7 +38,7 @@ def chooseCache(input_path, block_size=64, cache_size=32, cache_associativity=2,
         print("Your choice is Set Associative\n")
         number_of_sets = int(cache_size / (cache_associativity * block_size) * 1024)
         print("You have a {}-way cache of size {} kilobytes with {} set.".format(cache_associativity, cache_size, number_of_sets))
-        hit, miss = set_associative(binary_address_list, cache_associativity, block_size, number_of_sets, replacement_policy)
+        hit, miss, preprocess_time = set_associative(binary_address_list, cache_associativity, block_size, number_of_sets, replacement_policy)
 
     elif cache_associativity == 0:
         print("Your choice is Fully Associative\n")
@@ -45,12 +47,15 @@ def chooseCache(input_path, block_size=64, cache_size=32, cache_associativity=2,
         hit, miss = fully_associative(binary_address_list, number_of_blocks, block_size, replacement_policy)
     else:
         miss = hit = 'NAN'
-        print("Invalid Associativity.")
+        print("Invalid value for associativity!")
 
+    end_time = time.time()
     print("Results for {} records in L1{} file:".format(len(binary_address_list), input_path[-1]))
     print("Cache hit = ", hit)
     print("Cache miss = ", miss)
     print("So hit rate is equal to {}%".format(round(hit / (hit + miss) * 100, 2)))
+    print("Preprocessing the data took {} seconds".format(round(preprocess_time - start_time, 5)))
+    print("Runtime for this set of traces was {} seconds".format(round(end_time - preprocess_time, 5)))
 
 
 # step 2 - cache structures:
@@ -62,18 +67,17 @@ def set_associative(address_list, ways, block_size, num_of_sets, policy):
     # print("For first address, tag is {} , set index is {} , and block offset is {} .".format(address_list[0][:-(bo + st)],
     #                                                                                          address_list[0][-(st + bo):-bo],
     #                                                                                          address_list[0][-bo:]))
-    # next address list will be converted into a list of tuples for separation and faster process
-    processed_address_list = [(address[-(st + bo):-bo], address[:-(bo + st)], address[-bo:]) for address in address_list]
-    global pre_process_time  # this parameter is temporarily defined global.
+    # next address list will be converted into a tuple of tuples for separation and faster process
+    trace_list = tuple([(address[-(st + bo):-bo], address[:-(bo + st)], address[-bo:]) for address in address_list])
     pre_process_time = time.time()
     # DEBUG_AREA
-    # print("First address will be like: ", processed_address_list[0])
+    # print("First address will be like: ", trace_list[0])
     hit_count = 0
     miss_count = 0
+    cache = dict()
     if policy == "lru":
-        cache = dict()
         # cache is going to look like {"index1": {"tag1": lru_bit1, "tag2":lru_bit2}, "index2": {"tag1": lru_bit1, "tag2":lru_bit2}}
-        for address in processed_address_list:  # address sample [set_index, tag, block_offset]
+        for address in trace_list:  # address sample [set_index, tag, block_offset]
             is_index_cached = lookup(address[0], list(cache.keys()))  # is the requested index in cache or not?
             if is_index_cached:
                 is_tag_cached = lookup(address[1], list(cache[address[0]].keys()))  # is the requested tag is that cache index or not?
@@ -115,6 +119,7 @@ def set_associative(address_list, ways, block_size, num_of_sets, policy):
                         # print(cache[address[0]])
             else:
                 miss_count += 1
+                # DEBUG_AREA
                 # print("miss")
                 if len(cache) < num_of_sets:
                     cache[address[0]] = {address[1]: 0}
@@ -122,13 +127,59 @@ def set_associative(address_list, ways, block_size, num_of_sets, policy):
             # print(cache)
             # time.sleep(0.25)
 
-    elif policy == "belady":
-        print("Belady cache policy has not been implemented yet.")
-        pass
+    elif policy == "optimal":
+        # print("Optimal cache policy has not been implemented yet.")
+        for add_index in range(len(trace_list)):  # address sample [set_index, tag, block_offset]
+            # DEBUG_AREA
+            print(trace_list[add_index][0])
+            # is the requested index in cache or not?
+            is_index_cached = lookup(trace_list[add_index][0], list(cache.keys()))
+            if is_index_cached:
+                # is the requested tag is that cache index or not?
+                is_tag_cached = lookup(trace_list[add_index][1], list(cache[trace_list[add_index][0]].keys()))
+                # DEBUG_AREA
+                # print(cache[address[0]].keys())
+                if is_tag_cached:
+                    hit_count += 1
+                    # DEBUG_AREA
+                    # print("hit-hit")
+                    # print(len(cache[address[0]]))
+                    if cache[trace_list[add_index][0]][trace_list[add_index][1]] < (ways - 1):
+                        cache[trace_list[add_index][0]][trace_list[add_index][1]] += 1
+                        # here I have another job which is decreasing number of others by one.
+                else:
+                    miss_count += 1
+                    # DEBUG_AREA
+                    # print("hit-miss")
+                    if len(cache[address[0]]) < ways:  # if ways is 2, len can be 1, or 2.
+                        # DEBUG_AREA
+                        # print("Now we have these: ", cache[address[0]])
+                        # print("This is going to be added: ", [address[1], 0])
+                        cache[address[0]][address[1]] = 0
+                        # break
+                    else:
+                        # DEBUG_AREA
+                        # print("An item is going to be removed now:")
+                        lru_key = list(cache[address[0]].keys())[0]
+                        # DEBUG_AREA
+                        # print("This one: ", lru_key)
+                        del cache[address[0]][lru_key]
+                        # DEBUG_AREA
+                        # print(cache[address[0]])
+                        # break
+            else:
+                miss_count += 1
+                # DEBUG_AREA
+                print("miss")
+                if len(cache) < num_of_sets:
+                    cache[trace_list[add_index][0]] = {trace_list[add_index][1]: 0}
+            # DEBUG_AREA
+            time.sleep(2)
+
     else:
         print("Requested policy will not be supported in my code")
 
-    return hit_count, miss_count
+    return hit_count, miss_count, pre_process_time
 
 
 def direct_mapping(address_list, block_size, num_of_blocks):
@@ -147,10 +198,6 @@ def lookup(address, list_of_indices):
     # must return True (hit) or False (miss).
 
 
-# begin timing the code
-start_time = time.time()
+
 path_to_instructions_file = '/home/ali/Desktop/Projects/AliCache/00-L1i'
-chooseCache(path_to_instructions_file, block_size=64, cache_size=32, cache_associativity=8, replacement_policy="lru")
-end_time = time.time()
-print("Preprocessing the data took {} seconds".format(round(pre_process_time - start_time, 5)))
-print("Runtime for this set of traces was {} seconds".format(round(end_time - pre_process_time, 5)))
+chooseCache(path_to_instructions_file, block_size=64, cache_size=32, cache_associativity=2, replacement_policy="optimal")
