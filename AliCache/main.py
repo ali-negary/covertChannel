1e9dc380dfd64bd35f0b8341203f792a33a4f9e2
Ali Negary <ali.negary.76@gmail.com>
2021-03-25 01:27:09 +0430

    optimal policy is done. no issuess yet

--- AliCache/main.py
+++ AliCache/main.py
@@ -20,6 +20,7 @@ Please enter the input arguments as follows:
 # step 1 - define the main method:
 def chooseCache(input_path, block_size=64, cache_size=32, cache_associativity=2, replacement_policy="lru"):
     # begin timing the code
+    preprocess_time = 999999999999999
     start_time = time.time()
     input_file = open(input_path, 'r')
     hex_address_list = input_file.readlines()
@@ -32,7 +33,7 @@ def chooseCache(input_path, block_size=64, cache_size=32, cache_associativity=2,
         number_of_blocks = int(cache_size / block_size * 1024)
         print("Sorry. I have not worked on this part yet.")
         # print("You have a cache of size {} kilobytes with {} blocks.".format(cache_size, number_of_blocks))
-        hit, miss = direct_mapping(binary_address_list, block_size, number_of_blocks)
+        hit, miss, preprocess_time = direct_mapping(binary_address_list, block_size, number_of_blocks)
 
     elif cache_associativity > 1 & cache_associativity % 2 == 0 & cache_associativity <= 16:
         print("Your choice is Set Associative\n")
@@ -44,7 +45,7 @@ def chooseCache(input_path, block_size=64, cache_size=32, cache_associativity=2,
         print("Your choice is Fully Associative\n")
         print("Sorry. I have not worked on this part yet.")
         number_of_blocks = 1
-        hit, miss = fully_associative(binary_address_list, number_of_blocks, block_size, replacement_policy)
+        hit, miss, preprocess_time = fully_associative(binary_address_list, number_of_blocks, block_size, replacement_policy)
     else:
         miss = hit = 'NAN'
         print("Invalid value for associativity!")
@@ -98,7 +99,7 @@ def set_associative(address_list, ways, block_size, num_of_sets, policy):
                     if len(cache[address[0]]) < ways:  # if ways is 2, len can be 1, or 2.
                         # DEBUG_AREA
                         # print("Now we have these: ", cache[address[0]])
-                        # print("This is going to be added: ", [address[1], 0])
+                        # print("This is going to be added: ", {address[1]: 0})
                         cache[address[0]][address[1]] = 0
                         # break
                     else:
@@ -108,6 +109,7 @@ def set_associative(address_list, ways, block_size, num_of_sets, policy):
                         # DEBUG_AREA
                         # print("This one: ", lru_key)
                         del cache[address[0]][lru_key]
+                        # baad az inke ino hazf kardi, chizi mizari jash ya na? na, pas ridi :|
                         # DEBUG_AREA
                         # print(cache[address[0]])
                         # break
@@ -129,51 +131,68 @@ def set_associative(address_list, ways, block_size, num_of_sets, policy):
 
     elif policy == "optimal":
         # print("Optimal cache policy has not been implemented yet.")
-        for add_index in range(len(trace_list)):  # address sample [set_index, tag, block_offset]
-            optimal_forward_list = trace_list[add_index:add_index+1000]  # list sample [set_index, tag, block_offset]
+        for addr_index in range(len(trace_list)):  # address sample [set_index, tag, block_offset]
+            optimal_forward_list = trace_list[addr_index:addr_index + 10000]  # list sample [set_index, tag, block_offset]
+            forward_tag_list = [trace[1] for trace in optimal_forward_list if trace[0] == trace_list[addr_index][0]]  # tags with identical indices
             # DEBUG_AREA
-            print(trace_list[add_index][0])
-            is_index_cached = lookup(trace_list[add_index][0], list(cache.keys()))  # is the requested index in the cache or not?
+            if addr_index % 100000 == 0: print(addr_index)
+            # print(optimal_forward_list[:2])
+            # print(trace_list[addr_index][0])
+            is_index_cached = lookup(trace_list[addr_index][0], list(cache.keys()))  # is the requested index in the cache or not?
             if is_index_cached:
-                is_tag_cached = lookup(trace_list[add_index][1], list(cache[trace_list[add_index][0]].keys()))  # is the requested tag in the index?
+                is_tag_cached = lookup(trace_list[addr_index][1], list(cache[trace_list[addr_index][0]].keys()))  # is the requested tag in the index?
                 # DEBUG_AREA
-                # print(cache[trace_list[add_index][0]].keys())
+                # print(cache[trace_list[addr_index][0]].keys())
                 if is_tag_cached:
                     hit_count += 1
                     # DEBUG_AREA
                     # print("hit-hit")
-                    # print(len(cache[address[0]]))
-                    if cache[trace_list[add_index][0]][trace_list[add_index][1]] < (ways - 1):
-                        cache[trace_list[add_index][0]][trace_list[add_index][1]] += 1
-                        # here I have another job which is decreasing number of others by one.
                 else:
                     miss_count += 1
                     # DEBUG_AREA
                     # print("hit-miss")
-                    if len(cache[address[0]]) < ways:  # if ways is 2, len can be 1, or 2.
+                    if len(cache[trace_list[addr_index][0]]) < ways:  # if value of ways is 2, here len can be 1, or 2.
                         # DEBUG_AREA
-                        # print("Now we have these: ", cache[address[0]])
-                        # print("This is going to be added: ", [address[1], 0])
-                        cache[address[0]][address[1]] = 0
+                        # print("Now we have these: ", cache[trace_list[addr_index][0]])
+                        # print("This is going to be added: ", {trace_list[addr_index][1]: 0})
+                        cache[trace_list[addr_index][0]][trace_list[addr_index][1]] = 0
                         # break
                     else:
+                        remove_candidates = cache[trace_list[addr_index][0]].copy()
                         # DEBUG_AREA
-                        # print("An item is going to be removed now:")
-                        lru_key = list(cache[address[0]].keys())[0]
+                        # print("an item is going to be removed now:")
+                        # print("remove candidates are: ", remove_candidates)
+                        for value in remove_candidates:
+                            # DEBUG_AREA
+                            # print("value: ", value)
+                            if value not in forward_tag_list:
+                                # DEBUG_AREA
+                                # print("tag is not indexed. the range of forward list is not wide enough...")
+                                pass
+                            else:
+                                # DEBUG_AREA
+                                remove_candidates[value] = forward_tag_list.index(value)
+                                # print("index of value in forward list: ", forward_tag_list.index(value))
+                        remove_choice = min(remove_candidates, key=remove_candidates.get)
                         # DEBUG_AREA
-                        # print("This one: ", lru_key)
-                        del cache[address[0]][lru_key]
+                        # print("cache set before pop", remove_candidates)
+                        # print("our choice to be removed: ", remove_choice)
+                        del cache[trace_list[addr_index][0]][remove_choice]
                         # DEBUG_AREA
-                        # print(cache[address[0]])
+                        # print("cache set after pop", cache[trace_list[addr_index][0]])
+                        cache[trace_list[addr_index][0]][trace_list[addr_index][1]] = 0
+                        # cache[trace_list[addr_index][0]].update({trace_list[addr_index][1]: 0})  # same result as the above line but a bit strange
+                        # DEBUG_AREA
+                        # print("cache set after new push", cache[trace_list[addr_index][0]])
                         # break
             else:
                 miss_count += 1
                 # DEBUG_AREA
-                print("miss")
+                # print("miss")
                 if len(cache) < num_of_sets:
-                    cache[trace_list[add_index][0]] = {trace_list[add_index][1]: 0}
+                    cache[trace_list[addr_index][0]] = {trace_list[addr_index][1]: 0}
             # DEBUG_AREA
-            time.sleep(2)
+            # time.sleep(0.1)
 
     else:
         print("Requested policy will not be supported in my code")
@@ -182,13 +201,15 @@ def set_associative(address_list, ways, block_size, num_of_sets, policy):
 
 
 def direct_mapping(address_list, block_size, num_of_blocks):
+    pre_process_time = time.time()
     hit = miss = 0
-    return hit, miss
+    return hit, miss, pre_process_time
 
 
 def fully_associative(address_list, num_of_blocks, block_size, policy):
+    pre_process_time = time.time()
     hit = miss = 0
-    return hit, miss
+    return hit, miss, pre_process_time
 
 
 # step 3 - lookup method:
